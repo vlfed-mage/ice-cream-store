e2346d4dc7141066de119545f940a0e7f922b3ff
Valdo <gkorchun@gmail.com>
2023-02-15 17:35:38 +0200

    Setup images component

---
+++ src/components/ice-cream-image/ice-cream-image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const IceCreamImage = ({ iceCreamId }) =>
+    iceCreamId != null && (
+        <img
+            src={ require(`../../images/ice-cream-images/ice-cream-${ iceCreamId }.svg`) }
+            alt=''/>
+    );
+
+IceCreamImage.propTypes = {
+    iceCreamId: PropTypes.number.isRequired
+}
+
+export default IceCreamImage;
