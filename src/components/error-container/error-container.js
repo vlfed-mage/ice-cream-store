59b02d93ffbb583a21dee1ac393537ef42769f1f
Valdo <gkorchun@gmail.com>
2023-03-01 14:18:05 +0200

    Provide accessible validation

--- src/components/error-container/error-container.js
+++ src/components/error-container/error-container.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './error-container.scss';
 
-const ErrorContainer = ({ errorText, children, submited }) => {
+const ErrorContainer = ({ errorText, children, submited, errorId }) => {
     return (
         <div className={errorText && submited ? 'error' : null}>
             {children}
-            <div className='error-wrapper'>{errorText && submited && <span>{errorText}</span>}</div>
+            <div className='error-wrapper'>{errorText && submited && <span id={errorId}>{errorText}</span>}</div>
         </div>
     );
 };
--- src/components/hooks/use-validation.js
+++ src/components/hooks/use-validation.js
@@ -1,13 +1,21 @@
 import { useEffect, useState } from 'react';
 
-const useValidation = (value, validationFn, compareValue = null) => {
+const useValidation = (value, errorId, showError, validationFn, isRequired = true, compareValue = null) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
         setError(validationFn(value, compareValue));
     }, [value, validationFn, compareValue]);
 
-    return error;
+    return [
+        error,
+        {
+            'aria-discribedby': error && showError ? errorId : null,
+            'aria-invalid': error && showError ? 'true' : 'false',
+            'aria-required': isRequired ? 'true' : null,
+            required: isRequired,
+        },
+    ];
 };
 
 export default useValidation;
